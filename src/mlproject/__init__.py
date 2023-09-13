c3fa3d4278e3f7f01bc2f6370adcfb04693ab1f7
Satyam Sharma <55931484+codedestructed007@users.noreply.github.com>
2023-09-13 12:15:55 +0530

    Updated logs

--- src/mlproject/__init__.py
+++ src/mlproject/__init__.py
@@ -2,23 +2,20 @@ import logging
 import os
 import sys
 
-## create directory for logs
+# create directory for logs
 log_dirs = "logs"
-log_filepath = os.path.join(log_dirs,"running_logs.log")
-os.makedirs(log_dirs,exist_ok=True)
+log_filepath = os.path.join(log_dirs, "running_logs.log")
+os.makedirs(log_dirs, exist_ok=True)
 
 # Configure the logging settings
 logging.basicConfig(
-    filename='custom.log',  # Specify the log file name
     level=logging.INFO,
     format='%(asctime)s - %(module)s - %(levelname)s - %(message)s',
     handlers=[
-        logging.FileHandler(log_filepath), ## for writing log in the file
-        logging.StreamHandler(sys.stdout) ## for terminal
+        logging.FileHandler(log_filepath),  ## for writing log in the file
+        logging.StreamHandler(sys.stdout)  ## for terminal
     ]
 )
-## our custom logger
-logger = logging.getLogger('mlProjectLogger')
+# our custom logger
 
-# Create a custom logger
-logger = logging.getLogger('my_custom_logger')
+logger = logging.getLogger('mlProjectLogger')
